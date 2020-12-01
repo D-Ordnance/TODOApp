2e4d1bb98f9b92f4a35abbeb9c8bde2e2e9e5116
Deeosoft <dolapo@ridetru.com>
2020-12-01 13:22:38 +0100

    hamcrest assertion framework

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -2,6 +2,8 @@ package com.example.android.architecture.blueprints.todoapp.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import junit.framework.TestCase
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
 class StatisticsUtilsTest : TestCase(){
@@ -17,10 +19,16 @@ class StatisticsUtilsTest : TestCase(){
         val result = getActiveAndCompletedStats(tasks)
 
         //Check your result
-        assertEquals(result.activeTasksPercent, 50f)
-        assertEquals(result.completedTasksPercent, 50f)
-        assertEquals(result.isActiveCount, 1)
-        assertEquals(result.isCompletedCount, 1)
-        assertEquals(result.allTasks, 2)
+//        assertEquals(result.activeTasksPercent, 50f)
+//        assertEquals(result.completedTasksPercent, 50f)
+//        assertEquals(result.isActiveCount, 1)
+//        assertEquals(result.isCompletedCount, 1)
+//        assertEquals(result.allTasks, 2)
+        //Check your result using hamcrest matcher test framework
+        assertThat(result.activeTasksPercent, `is`(50f))
+        assertThat(result.completedTasksPercent, `is`(50f))
+        assertThat(result.isActiveCount, `is`(1))
+        assertThat(result.isCompletedCount, `is`(1))
+        assertThat(result.allTasks, `is`(2))
     }
 }
